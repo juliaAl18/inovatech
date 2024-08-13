6b5533a80f82ddd5af669f75024ddcacca52fd25
henriqtorresl <henriquetlandin@gmail.com>
2024-08-13 14:33:59 -0300

    why choose us responsiva e com animações

--- src/app/modules/home/why-choose-us/why-choose-us.component.ts
+++ src/app/modules/home/why-choose-us/why-choose-us.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-why-choose-us',
   templateUrl: './why-choose-us.component.html',
   styleUrls: ['./why-choose-us.component.scss'],
 })
-export class WhyChooseUsComponent {
+export class WhyChooseUsComponent implements AfterViewInit {
 
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.observeElementAnimation('#upper-benefit-left', 'comes-from-down');
+    this.observeElementAnimation('#upper-benefit-right', 'comes-from-down');
+    this.observeElementAnimation('#lower-benefit', 'comes-from-down');
+  }
+
+  observeElementAnimation(elementSelector: string, animationClass: string): void {
+    const element = document.querySelector(elementSelector);
+
+    if (element) {
+      // Adiciona a classe "hidden" inicialmente
+      element.classList.add('hidden');
+
+      const observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          entries[0].target.classList.remove('hidden');
+          entries[0].target.classList.add(animationClass);
+        }
+      }, {
+        threshold: 0.2
+      });
+
+      observer.observe(element);
+    }
+  }
+  
 }
